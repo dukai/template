403c3d3b8d2830c57dad3d71af5ea56bdf1d6e94
dukai <dukai86@gmail.com>
2016-04-25 10:26:55 +0800

    fixed bug

--- tools.js
+++ tools.js
@@ -101,40 +101,40 @@ define(function(require, exports, module){
     };
     exports.indexOf = indexOf;
 
-    var eles = {
-        div: document.createElement('div'),
-        ul: document.createElement('ul'),
-        li: document.createElement('li'),
-        span: document.createElement('span'),
-        p: document.createElement('p'),
-        a: document.createElement('a'),
-        fragment: document.createDocumentFragment(),
-        input: document.createElement('input')
-    }
-    /**
-     * create element
-     * @param tagName
-     * @param id
-     * @param className
-     * @returns {null}
-     */
-    var $c = function(tagName, id, className){
-        var ele = null;
-        if(!eles[tagName]){
-            eles[tagName] = document.createElement(tagName);
-            ele = eles[tagName].cloneNode(true);
-        }else{
-            ele = eles[tagName].cloneNode(true);
-        }
-        if(id){
-            ele.id = id;
-        }
-        if(className){
-            ele.className = className;
-        }
-        return ele;
-    };
-    exports.$c = $c;
+//    var eles = {
+//        div: document.createElement('div'),
+//        ul: document.createElement('ul'),
+//        li: document.createElement('li'),
+//        span: document.createElement('span'),
+//        p: document.createElement('p'),
+//        a: document.createElement('a'),
+//        fragment: document.createDocumentFragment(),
+//        input: document.createElement('input')
+//    }
+//    /**
+//     * create element
+//     * @param tagName
+//     * @param id
+//     * @param className
+//     * @returns {null}
+//     */
+//    var $c = function(tagName, id, className){
+//        var ele = null;
+//        if(!eles[tagName]){
+//            eles[tagName] = document.createElement(tagName);
+//            ele = eles[tagName].cloneNode(true);
+//        }else{
+//            ele = eles[tagName].cloneNode(true);
+//        }
+//        if(id){
+//            ele.id = id;
+//        }
+//        if(className){
+//            ele.className = className;
+//        }
+//        return ele;
+//    };
+//    exports.$c = $c;
 
     var rand = function (m, n) {
         return Math.floor((n - m + 1) * Math.random() + m);
