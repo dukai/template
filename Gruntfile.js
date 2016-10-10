d780a37e3ac5faf01198aea6dc2f5b8e8ebc1bbc
dukai <dukai86@gmail.com>
2016-10-10 14:33:40 +0800

    更新压缩方式

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,12 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     version: version,
+    concat: {
+      dist: {
+        src: ['lib/umd-header.js', '*.min.js'],
+        dest: 'template-<%= version %>.min.js',
+      },
+    },
     nodeunit: {
       files: ['test/**/*_test.js'],
     },
@@ -37,10 +43,27 @@ module.exports = function(grunt) {
         tasks: ['jshint:test', 'nodeunit']
       },
     },
+    requirejs: {
+      compile: {
+        options: {
+          // optimize: 'none',
+          // skipDirOptimize: true,
+          baseUrl: './',
+          name: 'template',
+          out: "./template-<%= version %>.min.js",
+          paths: {
+            dtools: 'empty:',
+            jquery: 'empty:',
+          },
+          exclude: ['dtools']
+        }
+      }
+    },
+
     uglify: {
       my_target: {
         files: {
-          './template-<%=version%>.min.js': ['template.js']
+          './template-<%=version%>.min.js': ['*.min.js']
         }
       }
     }
@@ -48,12 +71,14 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
-  grunt.registerTask('release', ['uglify']);
+  grunt.registerTask('release', ['requirejs', 'concat', 'uglify']);
 
 };
